d9549c6ea43843bf0bd4977a0cdac3e997dd7f6d
Christabel00 <134796750+Christabel00@users.noreply.github.com>
2024-03-28 12:42:53 +0200

    Add files via upload

---
+++ chapter2.js
@@ -0,0 +1,165 @@
+
+// // Exercise 2.1
+// let str1='Laurence';
+// console.log(typeof (str1));
+
+// let str2="Svekis";
+// console.log(typeof(str2));
+
+// let val1=undefined;
+// console.log(typeof(undefined));
+
+// let val2= null;
+// console.log(typeof(null));
+
+// let myNum =1000;
+// console.log(typeof(myNum));
+
+// //ADDITION
+// let nr1=12;
+// let nr2=14;
+// let result1=nr1 + nr2;
+// alert("The result is " + result1);
+
+// //concatenating Strings with a +
+// let str1 ="Hello ";
+// let str2="addition";
+// let result2 = str1 + str2;
+// alert(result2);
+
+
+// //EXERCISE 2.2
+// let name ="Ruvimbo";
+// let age =23;
+// let canYou= true;
+
+// alert("My name is "+ name +","+ "I am " + age +" And i can code with js :" + canYou);
+
+// //SUBTRACTION
+// let nr1 = 20;
+// let nr2 = 4;
+// let str1 = "Hi";
+// let nr3 = 3;
+// let result1 = nr1 - nr2;
+// let result2 = str1 - nr3;
+// console.log(result1, result2);
+
+// //MULTIPLICATION
+// let nr1 = 15;
+// let nr2 = 10;
+// let str1 = "Hi";
+// let nr3 = 3;
+// let result1 = nr1 * nr2;
+// let result2 = str1 * nr3;
+// console.log(result1, result2);
+
+//DIVISION
+// let nr1 = 30;
+// let nr2 = 5;
+// let result1 = nr1 / nr2;
+// console.log(result1);
+
+// //EXPONENTIATION
+// let nr1 = 2;
+// let nr2 = 3;
+// let result1 = nr1 ** nr2;
+// alert(result1);
+
+// //MODULUS
+// let nr1 =10;
+// let nr2 =3;
+// let result1 =nr1 % nr2;
+// alert(result1);
+
+// let nr3 = 8;
+// let nr4 = 2;
+// let result2 = nr3 % nr4;
+// alert(result2);
+
+// let nr5 = 15;
+// let nr6 = 4;
+// let result3 = nr5 % nr6;
+// alert(result3);
+
+// //UNARY OPERATORS:INCREMENT AND DECREMENT
+// let nr1 = 4;
+// nr1++;
+// alert(nr1);
+
+// let nr2 = 4;
+// nr2--;
+// alert(nr2);
+
+// //EXERCISE 2.4
+// let a = 4;
+// let b = 9;
+// let c = 16;
+
+
+// alert(b+=a);
+// alert(a/=c);
+// let answer= c%b;
+// c=answer;
+// alert (answer);
+
+
+// //COMPARISON OPERATOR
+// //EQUAL
+// let x = 5;
+// let y = "5";
+// console.log(x == y);//true
+// console.log(x===y);//false
+
+// //NOT EQUAL
+// let x = 5;
+// let y = "5";
+// alert(x !== y);//true
+// alert(x!=y);//false
+
+// //GREATER THAN AND SMALLER THAN
+// let x = 5;
+// let y = 6;
+// alert(y > x); 
+
+// alert(y >= y);
+
+// //LOGICAL OPERATORS
+// //AND
+// let x = 1;
+// let y = 2;
+// let z = 3;
+// alert(x < y && y < z);//true
+
+// alert(x > y && y < z);//false
+
+//OR
+//alert(x > y || y < z);//one expression has to be true
+
+//NOT
+// let x = false; //fliping a boolean 
+// alert(!x);
+
+// let x = 1;
+// let y = 2;
+// alert(!(x < y)); //false
+
+//Project
+
+// //Miles to km 
+// let mile = 60;
+// let km= 1.60934;
+// result= mile * km;
+// alert("The distance of " + result +" km " +"is equal to 60 miles");
+
+// //BMI CALCULATOR
+// let inch =60;
+// let cm =2.54;
+// let pounds= 250;
+// let kilo=1;
+
+// let height = inch*cm;
+// let weight = 2.2046*pounds;
+
+// alert(height);
+// alert(weight);
+
